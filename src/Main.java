73f4f6ca2032acdafc3643770aa66982a59d0ba1
ANIL CAN ÖZDEMİR <48845109+anilcanozdemir@users.noreply.github.com>
2022-10-07 18:15:46 +0300

    ebebek Java&QA Practicum
    ebebek Java&QA Practicum

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee employee= new Employee("  ANIL",5000,41,2011);
+
+        employee.toString();
+        System.out.println("Hello world!");
+    }
+}
